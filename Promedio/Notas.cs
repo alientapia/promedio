4fd297db759f59aa331fab01ff7a6b94cb3d16e5
Alien Tapia Salvador <alientapia33@gmail.com>
2019-01-05 09:13:00 -0300

    Segundo commit

--- Promedio/Notas.cs
+++ Promedio/Notas.cs
@@ -8,5 +8,25 @@ namespace Promedio
 {
     class Notas
     {
+        private double nota1;
+        private double nota2;
+        private double nota3;
+        private double nota4;
+
+        public Notas()
+        {
+        }
+
+        public double Nota1 { get => nota1; set => nota1 = value; }
+        public double Nota2 { get => nota2; set => nota2 = value; }
+        public double Nota3 { get => nota3; set => nota3 = value; }
+        public double Nota4 { get => nota4; set => nota4 = value; }
+        public double Promedio()
+        {
+            double promedio;
+            promedio = (this.nota1 + this.nota2 + this.nota3 + this.nota4) / 4;
+            return promedio;
+        }
+
     }
 }
--- Promedio/Program.cs
+++ Promedio/Program.cs
@@ -10,6 +10,27 @@ namespace Promedio
     {
         static void Main(string[] args)
         {
+            double nota1;
+            double nota2;
+            double nota3;
+            double nota4;
+            double promedio;
+            Console.WriteLine("Entre la nota1:  ");
+            nota1 = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("Entre la nota2:  ");
+            nota2 = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("Entre la nota3:  ");
+            nota3 = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("Entre la nota4:  ");
+            nota4 = Convert.ToDouble(Console.ReadLine());
+            Notas nota = new Notas();
+            nota.Nota1 = nota1;
+            nota.Nota2 = nota2;
+            nota.Nota3 = nota3;
+            nota.Nota4 = nota4;
+            promedio = Convert.ToDouble( nota.Promedio());
+            Console.WriteLine("Su promedio es: " + promedio);
+            Console.ReadKey();
         }
     }
 }
